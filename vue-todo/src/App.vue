51e7ea09a073e9c053a8503aad7a51d71814ac04
santos019 <santos019@naver.com>
2022-06-21 17:55:16 +0900

    Lv.02

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList v-bind:propsdata="todoItems"></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
+
+
+export default {
+    data: function() {
+        return {
+            todoItems: []
+        }
+    },
+   created: function () {
+        if(localStorage.length > 0) {
+            for (let i=0; i<localStorage.length; i ++) {
+                if(localStorage.key(i) !== 'loglevel:webpack-dev-server')
+                    this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+            }
+        }
+    },
+  components: {
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoList': TodoList,
+    'TodoFooter': TodoFooter,
+  }
+} 
+</script>
+
+<style>
+body{
+  text-align: center;
+  background-color: #F6F6F6;
+}
+input {
+  border-style:groove;
+  width: 200px;
+}
+button {
+  border-style:groove;
+
+}
+.shadow{
+  box-shadow: 5px 10px 10px black;
+}
+</style>
---
+++ vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" class="inputDiv">
+    <span class="addContainer">
+      <i class="fas fa-plus addBtn"></i>
+    </span>
+  </div>
+</template>
+    <script src="https://kit.fontawesome.com/614602752e.js" crossorigin="anonymous"></script>
+<script>
+
+export default {
+  data: function() {
+    return {
+      newTodoItem:""
+    }
+  },
+  methods: {
+    addTodo: function() {
+      if(this.newTodoItem !== '') {
+      const obj ={completed: false, item: this.newTodoItem}
+      localStorage.setItem(this.newTodoItem, JSON.stringify(obj))
+      this.clearInput();
+          }
+
+},
+    clearInput: function () {
+      this.newTodoItem ='';
+    }
+  }
+}
+</script>
+
+<style scoped>
+.inputDiv{
+  width: 90%;
+}
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, blue, red);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: wheat;
+  vertical-align: middle;
+}
+</style>
