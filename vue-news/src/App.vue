e61502d25ed534954a6ce0a1e333cf6e152c5b64
santos019 <santos019@naver.com>
2022-06-23 18:13:18 +0900

    Lv.03 start

---
+++ vue-news/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <my-tool-bar></my-tool-bar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import MyToolBar from './components/MyToolbar.vue'
+export default {
+ components: {
+   MyToolBar,
+ },
+}
+</script>
+
+<style>
+
+</style>
