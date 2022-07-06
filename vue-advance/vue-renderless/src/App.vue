dadb8262cc84f0d253531fe38967597810eb4e77
santos019 <santos019@naver.com>
2022-07-06 16:33:24 +0900

    Lv.03 완강

---
+++ vue-advance/vue-renderless/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <fetch-data url="https://jsonplaceholder.typicode.com/users/1">
+        <div slot-scope="{ response, loading}" v-if="!loading">
+          {{response}}
+        </div>
+        <div v-else>
+          loading...
+        </div>
+      </fetch-data>
+    </div>
+</template>
+
+<script>
+import FetchData from './components/FetchData.vue'
+export default {
+  components: { FetchData },
+
+}
+</script>
+
+<style>
+
+</style>
